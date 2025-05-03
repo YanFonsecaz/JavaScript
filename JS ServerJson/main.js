b0217342781b99b6f33b45dd388e1b5ad652ae34
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-05-03 20:12:12 -0300

    Update main.js

--- JS ServerJson/main.js
+++ JS ServerJson/main.js
@@ -1,3 +1,7 @@
-fetch("http://localhost:3333/products").then(response => response.json()).then(data => console.log(data))
+fetch("http://localhost:3333/products")
+.then(response => response.json())
+.then(data => console.log(data))
 
-fetch(`http://localhost:3333/products/${id}`).then(response => response.json()).then(data => console.log(data))
+fetch(`http://localhost:3333/products/${id}`)
+.then(response => response.json())
+.then(data => console.log(data))
